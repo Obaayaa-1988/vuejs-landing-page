0d6774fceb447b403c408c7168249bd86cdfe9dd
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-07-17 01:59:43 +0100

    added styling for header

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,31 +3,31 @@
         <!-- <h1>{{ msg }}</h1> -->
 
         <!----Header section only-->
-        <header>
-
-            <div class="fashion-logo">
+        <header> 
+            <div class="nav-page">
+             <div class="fashion-logo">
                 <h2>Fashion</h2>
             </div>
 
-            <div class="nav-page">
                 <nav>
                     <ul>
-                        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-                        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-                        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-                        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-                        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+                        <li><a href="#Home.html" target="_blank" rel="noopener">Home</a></li>
+                        <li><a href="#Shop.html" target="_blank" rel="noopener">Shop</a></li>
+                        <li><a href="#Delivery.html" target="_blank" rel="noopener">Delivery</a></li>
+                        <li><a href="#About.html" target="_blank" rel="noopener">About</a></li>
+                        <li><a href="#Notification.html" target="_blank" rel="noopener">Notification</a></li>
                     </ul>
                 </nav>
+                 <div class="fashion-circle"></div>
 
             </div>
 
-            <div class="fashion-circle">Hello</div>
+           
 
         </header>
 
         <!---------section1 only-->
-        <div class="grid-main">
+        <!-- <div class="grid-main">
             <div class="fashion-name">
                 <h1>ASHBELLA <span>.</span></h1>
             </div>
@@ -62,7 +62,7 @@
             </div>
 
 
-        </div>
+        </div> -->
 
 
 
@@ -75,17 +75,59 @@
 <script>
 export default {
     name: 'LandingPage',
-    // props: {
-    //     msg: String
+    // data() {
+    //    return image: { {backgroundImage: "url(./assets/mage1.jpg)"}}
     // }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header {
+*{
+    padding: 0;
+    margin: 0;
+
+}
+header{
+    background-image: url('~@/assets/mage1.jpg');
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+    width: 100%;
+    /* object-fit: fill; */
+}
+
+.fashion-logo{
+    background-color:#EC7063;
+    color: white;
+    text-align: center;
+    margin-top: 2rem;
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+    
+}
+
+.fashion-circle{
+    /* background-color:#EC7063; */
+    background: #12c2e9;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #f64f59, #c471ed, #12c2e9);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #f64f59, #c471ed, #12c2e9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    color: white;
+    text-align: center;
+    height: 250px;
+    width: 250px;
+    border-radius: 50%;
+    /* position: absolute;
+    top: 80%;
+    left: 10% */
+}
+.nav-page {
     display: flex;
     justify-content: space-between;
+    position: relative;
 }
 
 
@@ -104,6 +146,6 @@ li {
 }
 
 a {
-    color: #42b983;
+    color: white;
 }
 </style>
