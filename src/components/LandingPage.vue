ef9a90ec14520acd250ca5f4f55c38e82644bd58
Obaayaa-1988 <aquaicoe31@gmail.com>
2022-09-25 21:55:32 +0100

    added router link

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,94 +1,138 @@
 <template>
-  <div>
-    <!----Header section only-->
-    <header>
-      <div class="nav-page">
-        <div class="fashion-logo"></div>
+    <div>
+        <!----Header section only-->
+        <header>
+            <div class="nav-page">
+                <div class="fashion-logo"></div>
 
-        <nav>
-            <ul><li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/shop">Shop</router-link></li>
-            <li><router-link to="/delivery">Delivery</router-link></li>
-            <li> <router-link to="/about">About</router-link></li>
-            <li><router-link to="/notification">Notification</router-link> </li>
-            </ul>
-          
-           <!-- <ul>
+                <nav>
+                    <ul>
+                        <li>
+                            <router-link to="/">Home</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/shop">Shop</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/delivery">Delivery</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/about">About</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/notification">Notification</router-link>
+                        </li>
+                    </ul>
+
+                    <!-- <ul>
             <li v-for="(link, index) in links" :key="index">
               <a :href="link.url">{{ link.name }}</a>
             </li>
           </ul>  -->
-        </nav>
+                </nav>
 
-        <!-- <div>
+                <!-- <div>
           <h1>{{ post.title }}</h1>
           <p>{{ post.numberOfLikes }}</p>
         </div> -->
 
-        <div class="fashion-circle"></div>
-      </div>
-
-      <div class="great">
-        <h1>GREATNESS</h1>
-      </div>
-      <div class="great2">
-        <button>Learn More</button>
-      </div>
-    </header>
-
-    <!---------section1 only-->
-    <div class="container">
-      <div class="main-grid">
-        <div class="fashion-name">
-          <h1>ASHBELLA .</h1>
-        </div>
-        <div class="grid-image">
-          <div class="image1">
-            <img src="../assets/mage11.jpg" />
-          </div>
-          <div class="image2"><img src="../assets/mage7.jpg" /></div>
-          <div class="image3"><img src="../assets/mage5.jpg" /></div>
-          <div class="image4"><img src="../assets/mage3.jpg" /></div>
-          <div class="image5"><img src="../assets/mage4.jpg" /></div>
-          <div class="image6"><img src="../assets/mage9.jpg" /></div>
-        </div>
-      </div>
-    </div>
-    <!-------card section-->
-    <section>
-      <h1>MILLENIUM GREATNESS</h1>
-      <div class="container">
-        <div class="main-card">
-          <div v-for="(image, index) in images" :key="index" class="card-inner">
-            <div><img :src="image" /></div>
-
-            <div class="card-bottom">
-              <button class="btn">Shop</button>
+                <div class="fashion-circle"></div>
             </div>
-          </div>
+
+            <div class="great">
+                <h1>GREATNESS</h1>
+            </div>
+            <div class="great2">
+                <button class="btn1">Learn More</button>
+            </div>
+        </header>
+
+        <!---------section1 only-->
+        <div class="container">
+            <div class="main-grid">
+                <div class="fashion-name">
+                    <h1>ASHBELLA .</h1>
+                </div>
+                <div class="grid-image">
+                    <div class="image1">
+                        <img src="../assets/mage11.jpg" />
+                    </div>
+                    <div class="image2"><img src="../assets/mage7.jpg" /></div>
+                    <div class="image3"><img src="../assets/mage5.jpg" /></div>
+                    <div class="image4"><img src="../assets/mage3.jpg" /></div>
+                    <div class="image5"><img src="../assets/mage4.jpg" /></div>
+                    <div class="image6"><img src="../assets/mage9.jpg" /></div>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
-    <div id="grid-container">
-    <div class="grid-color">
-        <h4>mi casa</h4>
+        <!-------card section-->
+        <section>
+            <h1>MILLENIUM GREATNESS</h1>
+            <div class="container">
+                <div class="main-card">
+                    <div v-for="(image, index) in images" :key="index" class="card-inner">
+                        <div><img :src="image" /></div>
 
+                        <div class="card-bottom">
+                            <button class="btn">Shop</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
+        <div id="grid-container">
+            <div class="grid-text" >
+                <h4>mi casa</h4>
+                <h2>TIMELESS FASHION</h2>
+                <p>Not Too Late</p>
+                
+            </div>
+
+            <div class="grid-color">
+                <h4>hello there</h4>
+                 <button class="btn2">Click Me</button>
+            </div>
+        </div>
+
+        <div id="grid2-container">
+
+             <div class="grid-color2">
+                <h4>hello there</h4>
+            </div>
+
+            <div class="grid-text2">
+                <h4>mi casa</h4>
+                <h2>TIMELESS FASHION</h2>
+                <p>Not Too Late</p>
+                <button class="btn1">Click Me</button>
+            </div>
+
+        </div>
+
+        <div id="grid3-container">
+
+             <div class="grid-text3">
+                <h4>mi casa</h4>
+                <h2>TIMELESS FASHION</h2>
+                <p>Not Too Late</p>
+                 <button class="btn1">Click Me</button>
+            </div>
+
+             <div class="grid-color3">
+                <h4>hello there</h4>
+            </div>
+
+        </div>
+
+        <!------footer section-->
+
+        <footer>
+            <p className="footer-para">
+                Powered By Walulel Ghana &copy; 2022 | Theme By: <span>Ashbella</span>
+            </p>
+        </footer>
     </div>
-    <div class="grid-text">
-        <h4>hello there</h4>
-
-    </div>
-
-    </div>
-    <!------footer section-->
-
-    <footer>
-      <p className="footer-para">
-        Powered By Walulel Ghana &copy; 2022 | Theme By: <span>Ashbella</span>
-      </p>
-    </footer>
-  </div>
 </template>
 
 <script>
@@ -99,28 +143,28 @@ import ShopComponent from "./ShopComponent.vue";
 // import Notification from "./components/Notification.vue";
 
 export default {
-  name: "LandingPage",
-  props: {
-    links: Array,
-    post: Object,
-  },
-  data() {
-    return {
-      images: [
-        require("../assets/mage10.jpg"),
-        require("../assets/mage1.jpg"),
-        require("../assets/mage8.jpg"),
-      ],
-    };
-  },
+    name: "LandingPage",
+    props: {
+        links: Array,
+        post: Object,
+    },
+    data() {
+        return {
+            images: [
+                require("../assets/mage10.jpg"),
+                require("../assets/mage1.jpg"),
+                require("../assets/mage8.jpg"),
+            ],
+        };
+    },
 
-  // components: {
-  //   Home,
-  ShopComponent,
-  // LiveComponent,
-  //   About,
-  //   Notification,
-  // },
+    // components: {
+    //   Home,
+    ShopComponent,
+    // LiveComponent,
+    //   About,
+    //   Notification,
+    // },
 };
 </script>
 
